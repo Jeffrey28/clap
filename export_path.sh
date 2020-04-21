5b50a0b42fe67cc90887b01c37c90215d2106843
zhcao <caoc11@foxmail.com>
2020-04-21 10:07:58 +0800

    first commit

--- export_path.sh
+++ export_path.sh
@@ -1,5 +1,5 @@
 # export UE4_ROOT=/home/yli/UnrealEngine
-export CARLA_ROOT=/home/zhong/carla-0.9.8
+export CARLA_ROOT=/home/carla/ZZZ/carla-098
 #export CARLA_ROOT=/home/yli/carla-0.9.5/Dist/CARLA_Shipping_0.9.5/LinuxNoEditor
 export CARLA_VERSION=0.9.8
 export PYTHONPATH=$CARLA_ROOT/PythonAPI/carla/dist/carla-${CARLA_VERSION}-py2.7-linux-x86_64.egg:$PYTHONPATH
